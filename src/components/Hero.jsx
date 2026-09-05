bface8857146a1a8ff127345bccfd11b5d249617
ranjan-fullstack <ranjansahusi84@gmail.com>
2026-09-05 20:28:28 +0530

    main

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,7 +10,7 @@ export default function Hero() {
     >
       <div className="absolute inset-0">
         <img
-          src="/images/background-image.jpeg"
+          src="/images/good.jpeg"
           alt=""
           className="h-full w-full object-cover object-right-top sm:object-right"
         />
